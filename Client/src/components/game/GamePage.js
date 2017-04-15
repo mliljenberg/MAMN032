663a55be62e4fe7e4c2fffc940a99c06a549e66f
Joel Buhrman <kbuh5442@uni.sydney.edu.au>
2017-04-15 17:58:27 +0200

    Påbörjat lite strukturering då jag märkt att många saker jag skapat kunde skapatas som egna React komponenter då de återanvänds med små ändringar och även för att göra strukturen lättare

--- Client/src/components/game/GamePage.js
+++ Client/src/components/game/GamePage.js
@@ -6,8 +6,9 @@ const socket = io.connect();
 import $ from 'jquery';
 import * as myselfAction from '../../actions/myselfAction';
 import {Link, IndexLink, browserHistory} from 'react-router';
+import DescriptionContainer from './DescriptionContainer';
+import PlayersContainer from './PlayersContainer';
 
-import Description from './Description';
 
 
 class GamePage extends React.Component {
@@ -25,10 +26,6 @@ class GamePage extends React.Component {
 
   }
 
-  buttonClicked() {
-    socket.emit('send message', window.location.href.substr(window.location.href.lastIndexOf('/') + 1));
-
-  }
 
   startAnimation() {
 
@@ -61,79 +58,19 @@ class GamePage extends React.Component {
   }
 
   render() {
-    const {players} = this.props;
-    let leftOverRow1 = "";
-    let leftOverClass = "horizontalListItem center-block col-xs-4"
-
-
-    this.state.leftOver = players.length % 3;
-    if (this.state.leftOver === 0) {
-      leftOverRow1 = <div className="col-xs-0"></div>;
-
-    }
-    if (this.state.leftOver === 1) {
-      leftOverRow1 = <div className="col-xs-0"></div>;
-      leftOverClass = "horizontalListItem myMargin center-block col-xs-3"
-
-    }
-    if (this.state.leftOver === 2) {
-      leftOverRow1 = <div className="col-xs-2"></div>;
-
-    }
 
 
       return (
         <div>
           <div id="descriptionContainer" className="hideFromStart">
-
-            <div className="col-xs-12">
-              <div className="col-xs-0 col-md-1"></div>
-              <div className="description col-xs-12 col-md-10">
-                <div className="col-xs-1"></div>
-                <div className="col-xs-10">
-                  <div className="myMargin"></div>
-
-                  {Description[0].description}<br/><br/>
-                  {Description[1].description}<br/><br/>
-                  {Description[2].description}
-                </div>
-                <div className="col-xs-1"></div>
-              </div>
-
-              <div className="col-xs-0 col-md-1"></div>
-            </div>
-            <div className="col-xs-0 col-md-3"></div>
-            <button className="myNeutralButton col-xs-12 col-md-6" onClick={this.startAnimation}>OK</button>
-            <div className="col-xs-0 col-md-3"></div>
+          <DescriptionContainer onClick={this.startAnimation}/>
           </div>
 
           <div id="playersContainer" className="hideFromStart">
+          <PlayersContainer onClick={this.leaveGame}/>
 
-            <div className="myMediumLargeText">Room: 1234</div>
-            <div className="col-xs-12">
-              <div className="col-xs-0 col-md-2"></div>
-              <div className="horizontalList col-xs-12 col-md-8">
-                {leftOverRow1}
-                {players.map(player =>
-                  <div className={leftOverClass} key={player.value}>
 
-                    <div className="mySmallText"> {player.value}</div>
 
-                  </div>
-                )}
-
-              </div>
-              <div className="col-xs-0 col-md-2"></div>
-            </div>
-
-            <div className="col-xs-12 myMargin">
-              <div className="col-md-3 col-xs-0"></div>
-
-              <Link to="/game">
-                <button className="myNeutralButton col-md-6 col-xs-12" onClick={this.leaveGame}>Leave</button>
-              </Link>
-              <div className="col-md-3 col-xs-0"></div>
-            </div>
           </div>
         </div>
       );
@@ -145,29 +82,5 @@ class GamePage extends React.Component {
 }
 
 
-GamePage.propTypes = {
 
-
-  myself: PropTypes.object.isRequired,
-  players: PropTypes.array.isRequired
-
-};
-
-function mapStateToProps(state, ownProps) {
-
-  return {
-
-    myself: state.myself,
-    players: state.players
-
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-
-    actions: bindActionCreators(Object.assign({}, myselfAction), dispatch)
-  };
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(GamePage);
+export default GamePage;
