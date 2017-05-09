428ad35981ac3594dd85e9abb2fe05f29801ee93
Joel Buhrman <kbuh5442@uni.sydney.edu.au>
2017-05-09 16:39:12 +0200

    La in 17 ord i lista, råkade bli på svenska

--- Client/tools/words.js
+++ Client/tools/words.js
@@ -1,14 +1,76 @@
 export default [
   {
-    word: "Snopp",
-    def: "Något Martin har 2 meter av."
+    word: "Abrasion",
+    def: "Havets nedbrytande verksamhet vid en kust"
   },
   {
-    word: "Vagina",
-    def: "Något Martin doppar i dagligen."
+    word: "Abbreviation",
+    def: "Förkortningstecken i medeltida handskrift eller notskrift"
   },
   {
-    word: "Stor Snopp",
-    def: "Något Joel vill ha."
+    word: "Bahytt",
+    def: "En sorts huvudbonad med band under hakan"
+  },
+  {
+    word: "Cachucha",
+    def: "En spansk dans"
+  },
+  {
+    word: "Camembert",
+    def: "En sorts finare fransk mjukosk"
+  },
+  {
+    word: "Debarkera",
+    def: "Stiga ur ett flygplan"
+  },
+  {
+    word: "Degeneration",
+    def: "Minskad funktionsduglighet hos t.ex en ras eller en släkt"
+  },
+  {
+    word: "Ebenist",
+    def: "Konstsnickare"
+  },
+  {
+    word: "Echaufferad",
+    def: "Upphetsad"
+  },
+  {
+    word: "Fajans",
+    def: "En sorts glaserat lergods"
+  },
+  {
+    word: "Famla",
+    def: "Känna sig för"
+  },
+  {
+    word:"Galeja",
+    def:"Ett alternativt ord för segelfartyg"
   }
+  ,
+  {
+    word:"Galghumor",
+    def:"Hånfull skämtsamhet"
+  }
+  ,
+  {
+    word: "Hamla",
+    def:"Kvista träd"
+  }
+  ,
+  {
+    word: "Hamstra",
+    def:"Köpa och lagra varor som det kan bli ont om"
+  }
+  ,
+  {
+    word:"Jamb",
+    def:"Tvåstavig versfot som består av trycksvag och därefter tryckstark stavelse"
+  }
+  ,
+  {
+    word:"Jalu",
+    def:"Svartsjuk"
+  }
+
 ];
