89e6c5d2135ab8a7a293a5c52302b0f78aed2d9e
Joel Buhrman <kbuh5442@uni.sydney.edu.au>
2017-05-09 14:57:57 +0200

    Fixat med description

--- Client/src/components/game/Description.js
+++ Client/src/components/game/Description.js
@@ -1,11 +1,18 @@
 export default [
+
   {
-    description: "This is a game of competence and trickyness."
+    description: "To win the game you must write the most believable description for the word, and guess on the true description."
   },
   {
-    description: "To win the game you must do this and that."
+    description: "The one with the most votes gets the most points. However, if you vote on the true description, you get the points! "
   },
   {
-    description: "You're awarded points for this and that.You're awarded points for this and that.You're awarded points for this and that.You're awarded points for this and that.You're awarded points for this and that."
+    description: "- Someone guesses on your description when you're not the rapper: 1p / vote"
+  },
+  {
+    description: "- Your guess is the true description: 2p"
+  },
+  {
+    description: "First one to 15 points wins!"
   }
 ];
--- Client/src/components/game/DescriptionContainer.js
+++ Client/src/components/game/DescriptionContainer.js
@@ -10,12 +10,15 @@ class DescriptionContainer extends React.Component {
           <div className="col-xs-0 col-md-1"></div>
           <div className="description col-xs-12 col-md-10">
             <div className="col-xs-1"></div>
-            <div className="col-xs-10">
-              <div className="myMargin"></div>
+            <div className="col-xs-10 descriptionInput">
+
 
               {Description[0].description}<br/><br/>
               {Description[1].description}<br/><br/>
-              {Description[2].description}
+              {Description[2].description}<br/><br/>
+              {Description[3].description}<br/>
+              {Description[4].description}<br/><br/>
+
             </div>
             <div className="col-xs-1"></div>
           </div>
