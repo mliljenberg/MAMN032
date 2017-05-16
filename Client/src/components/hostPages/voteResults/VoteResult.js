8a047d2941d1c742d8daa4a3748dc03ea534bb14
Joel Buhrman <kbuh5442@uni.sydney.edu.au>
2017-05-16 13:39:07 +0200

    VoteResults klart på hostsidan

--- Client/src/components/hostPages/voteResults/VoteResult.js
+++ Client/src/components/hostPages/voteResults/VoteResult.js
@@ -1,79 +1,80 @@
-import React from 'react';
+import React, {PropTypes}  from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import $ from 'jquery';
 import {browserHistory} from 'react-router';
+import * as wordAction from '../../../actions/wordAction';
 class VoteResult extends React.Component {
 
 
-
-
   componentDidMount() {
-
-    var rightAnswer = 3;
-
-
-    //Här skrivs alla svar ut, fadade om det inte är det riktiga, ofadade om det är det riktiga
-    for (var i = 0; i < 4; i++) {
-      if (i != rightAnswer) {
-        $("#allVotes").append($("<div/>",{class: 'col-md-6 col-xs-12 ', id: "voteBox"+i})
-        );
-
-        $("#voteBox"+i).append($("<div/>", {
-          class: 'voteBoxUnhiddenWrongAnswer'
-        }).append($("<div/>", {
-          class: 'centeredText voteBoxText',
-          text: 'HEJHÅ'
-        })).append($("<div/>", {
-          class: 'authorName',
-          text: 'Joel'
-        })));
+    /*
+     var rightAnswer = 3;
 
 
-        //Här sätter man stämplar på svaret med namn på alla som röstat på det
-        let votesOnThisAnswer=3;
-        for(var j=1; j<=votesOnThisAnswer;j++){
-          $("#voteBox"+i).append($("<div/>", {
-            class: 'voteStamp stampMargin'+j
-          }).append($("<div/>", {
-            class: 'voteStampText',
-            text: 'Ludde'
-          })));
-        }
+     //Här skrivs alla svar ut, fadade om det inte är det riktiga, ofadade om det är det riktiga
+     for (var i = 0; i < 4; i++) {
+     if (i != rightAnswer) {
+     $("#allVotes").append($("<div/>",{class: 'col-md-6 col-xs-12 ', id: "voteBox"+i})
+     );
+
+     $("#voteBox"+i).append($("<div/>", {
+     class: 'voteBoxUnhiddenWrongAnswer'
+     }).append($("<div/>", {
+     class: 'centeredText voteBoxText',
+     text: 'HEJHÅ'
+     })).append($("<div/>", {
+     class: 'authorName',
+     text: 'Joel'
+     })));
 
 
-      }
-
-      else {
-
-        $("#allVotes").append($("<div/>",{class: 'col-md-6 col-xs-12 ', id: "voteBox"+i})
-        );
-
-        $("#voteBox"+i).append($("<div/>", {
-          class: 'voteBoxUnhidden'
-        }).append($("<div/>", {
-          class: 'centeredText voteBoxText',
-          text: 'HEJHÅ'
-        })).append($("<div/>", {
-          class: 'authorName',
-          text: 'Joel'
-        })));
+     //Här sätter man stämplar på svaret med namn på alla som röstat på det
+     let votesOnThisAnswer=3;
+     for(var j=1; j<=votesOnThisAnswer;j++){
+     $("#voteBox"+i).append($("<div/>", {
+     class: 'voteStamp stampMargin'+j
+     }).append($("<div/>", {
+     class: 'voteStampText',
+     text: 'Ludde'
+     })));
+     }
 
 
-        //Här sätter man stämplar på svaret med namn på alla som röstat på det
-        let votesOnThisAnswer=3;
-        for(var j=1; j<=votesOnThisAnswer;j++){
-          $("#voteBox"+i).append($("<div/>", {
-            class: 'voteStamp stampMargin'+j
-          }).append($("<div/>", {
-            class: 'voteStampText',
-            text: 'Ludde'
-          })));
-        }
+     }
 
-      }
+     else {
 
-    }
+     $("#allVotes").append($("<div/>",{class: 'col-md-6 col-xs-12 ', id: "voteBox"+i})
+     );
+
+     $("#voteBox"+i).append($("<div/>", {
+     class: 'voteBoxUnhidden'
+     }).append($("<div/>", {
+     class: 'centeredText voteBoxText',
+     text: 'HEJHÅ'
+     })).append($("<div/>", {
+     class: 'authorName',
+     text: 'Joel'
+     })));
 
 
+     //Här sätter man stämplar på svaret med namn på alla som röstat på det
+     let votesOnThisAnswer=3;
+     for(var j=1; j<=votesOnThisAnswer;j++){
+     $("#voteBox"+i).append($("<div/>", {
+     class: 'voteStamp stampMargin'+j
+     }).append($("<div/>", {
+     class: 'voteStampText',
+     text: 'Ludde'
+     })));
+     }
+
+     }
+
+     }
+
+     */
 
     $("#theWordMeans").slideToggle("slow", function () {
       setTimeout(function () {
@@ -97,13 +98,19 @@ class VoteResult extends React.Component {
   }
 
   render() {
+
+
     return (
       <div id="totalContainer">
-        <div className="mySmallText hideFromStart" id="theWordMeans">"The word" means</div>
+        <div className="mySmallText hideFromStart" id="theWordMeans">"{this.props.word.word}" means</div>
         <div className="col-xs-12 voteBoxSingle" id="voteBox">
-          <div className="centeredText voteBoxText">HEJ</div>
+          <div className="centeredText voteBoxText">{this.props.word.def}</div>
         </div>
         <div id="allVotes" className="hideFromStart">
+          {this.props.answers.map(answer =>
+            <GetRightOrWrongAnswer votes={answer.voted} def={this.props.word.def} answer={answer}/>
+          )
+          }
         </div>
       </div>
 
@@ -111,4 +118,83 @@ class VoteResult extends React.Component {
   }
 }
 
-export default VoteResult;
+VoteResult.propTypes = {
+  actions: PropTypes.object.isRequired,
+  word: PropTypes.object.isRequired,
+  answers: PropTypes.array.isRequired
+
+  //myprop: PropTypes.string.isRequired
+
+};
+
+function mapStateToProps(state, ownProps) {
+  /**
+   let word = state.word;
+   if(!state.word.word){
+  word.word = "fel";
+}
+   */
+
+  return {
+    word: state.word,
+    answers: state.answers
+
+  };
+}
+function mapDispatchToProps(dispatch) {
+
+  return {
+
+    actions: bindActionCreators(Object.assign({}, wordAction), dispatch)
+  };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(VoteResult);
+
+
+function GetRightOrWrongAnswer(props) {
+
+
+  if (props.answer.answer == props.def) {
+
+
+
+    return (
+      <div className="col-md-6 col-xs-12" id={"voteBox" + props.answer.answer}>
+
+        <div className="voteBoxUnhidden">
+          <div className="centeredText voteBoxText">{props.answer.answer}</div>
+        </div>
+        <div className="authorName">{props.answer.username}</div>
+        {props.votes.map(vote =>
+
+          <div className={"voteStamp stampMargin"+props.votes.indexOf(vote)} id={vote} >
+           <div className="voteStampText">{vote}</div>
+          </div>
+        )
+        }
+
+      </div>);
+
+  }
+  else {
+    return (
+      <div className="col-md-6 col-xs-12" id={"voteBox" + props.answer.answer}>
+
+        <div className="voteBoxUnhiddenWrongAnswer">
+          <div className="centeredText voteBoxText">{props.answer.answer}</div>
+        </div>
+        <div className="authorName">{props.answer.username}</div>
+        {props.votes.map(vote =>
+
+          <div className={"voteStamp stampMargin"+props.votes.indexOf(vote)} id={vote} >
+            <div className="voteStampText">{vote}</div>
+          </div>
+        )
+        }
+      </div>
+    );
+  }
+
+}
